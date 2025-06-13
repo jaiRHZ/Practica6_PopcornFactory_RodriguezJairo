f878b6d9929f09c6e89347c543732ffc796b0dbe
jaiRHZ <jairo.rodriguez213248@potros.itson.edu.mx>
2025-06-13 15:51:37 -0700

    Primera version
    Aun no es funcional

---
+++ app/src/main/java/rodriguez/jairo/popcornfactory_213248/Catalog.kt
@@ -0,0 +1,20 @@
+package rodriguez.jairo.popcornfactory_213248
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Catalog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_catalog)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/rodriguez/jairo/popcornfactory_213248/Pelicula.kt
@@ -0,0 +1,7 @@
+package rodriguez.jairo.popcornfactory_213248
+
+data class Pelicula (
+    var titulo: String,
+    var image: Int,
+    var header: Int,
+    var sinopsis: String)
